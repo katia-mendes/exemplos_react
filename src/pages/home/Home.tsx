82f6e152c9a43a1ae25c4345e43967692cd571c1
Katia Mendes <katia.mendesx@gmail.com>
2023-11-07 07:04:47 -0300

    subindo tailwind

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,48 +1,22 @@
-import { useState } from "react"
-
 function Home() {
-    const [isLogged, setIsLogged] = useState(false)
-
     return (
         <>
-            {
-                isLogged ? (
-                    <div style={{
-                        width: "100vw",
-                        display: "flex",
-                        justifyContent: "center"
-                    }}>
-                        <div>
-                            <div style={{
-                                 width: "80vw",
-                                 display: "flex",
-                                 flexDirection: "column",
-                                 alignItems: "center"
-                            }}>
-                                <h2>Seja Bem Vinde!</h2>
-                                <p>Expresse aqui seus pensamentos e opniões</p>
-                            </div>
-        
-                            <div style={{
-                                 width: "80vw",
-                                 display: "flex",
-                                 flexDirection: "column",
-                                 alignItems: "center"
-                            }}>
-                                <img 
-                                    src="https://i.imgur.com/VpwApCU.png" 
-                                    alt="Imagem da Página Home" 
-                                    width="400px"
-                                />
-                            </div>
-                        </div>
+            <div className="w-screen flex justify-center">
+                <div>
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <h2>Seja Bem Vinde!</h2>
+                        <p>Expresse aqui seus pensamentos e opniões</p>
                     </div>
-                ) : (
 
-                    <button onClick={() => setIsLogged(true)}>Entrar</button>
-
-                )
-            }
+                    <div className="max-w-7xl flex flex-col items-center">
+                        <img
+                            src="https://i.imgur.com/VpwApCU.png"
+                            alt="Imagem da Página Home"
+                            width="400px"
+                        />
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
